8bc25c39175df5d2c0ee775fc0ee00ff8b86257b
Jeff Hagkull <jeff@hagkull.com>
2015-06-02 22:40:23 -0700

    Commit 11-1
    Added basic notifications collection.

--- server/publications.js
+++ server/publications.js
@@ -6,3 +6,7 @@ Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postID});
 });
+
+Meteor.publish('notifications', function() {
+  return Notifications.find();
+});
